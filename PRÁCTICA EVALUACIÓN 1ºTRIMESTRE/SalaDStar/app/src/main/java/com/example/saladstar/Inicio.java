cedd6128b9d8c85a0d5b42b4362cce34dbfaa78f
Alejandro1203 <alefg2003@gmail.com>
2024-12-05 13:05:36 +0100

    Inicio
    Pantalla de inicio de la aplicación y pantalla de Log In sin terminar

---
+++ PRÁCTICA EVALUACIÓN 1ºTRIMESTRE/SalaDStar/app/src/main/java/com/example/saladstar/Inicio.java
@@ -0,0 +1,34 @@
+package com.example.saladstar;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ProgressBar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Inicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inicio);
+
+        final ProgressBar carga = findViewById(R.id.carga);
+        carga.getIndeterminateDrawable().setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_IN);
+
+        Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Inicio.this, LogIn.class);
+                startActivity(intent);
+            }
+        }, 2000);
+
+
+    }
+}
