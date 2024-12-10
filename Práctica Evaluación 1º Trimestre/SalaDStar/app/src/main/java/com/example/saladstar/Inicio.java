38c93a2d57b70d225fbd25327ea48a9b2a887166
Alejandro1203 <alefg2003@gmail.com>
2024-12-10 14:38:19 +0100

    (Register y LogIn)

--- Práctica Evaluación 1º Trimestre/SalaDStar/app/src/main/java/com/example/saladstar/Inicio.java
+++ Práctica Evaluación 1º Trimestre/SalaDStar/app/src/main/java/com/example/saladstar/Inicio.java
@@ -28,7 +28,5 @@ public class Inicio extends AppCompatActivity {
                 startActivity(intent);
             }
         }, 2000);
-
-
     }
 }
--- Práctica Evaluación 1º Trimestre/SalaDStar/app/src/main/java/com/example/saladstar/LogIn.java
+++ Práctica Evaluación 1º Trimestre/SalaDStar/app/src/main/java/com/example/saladstar/LogIn.java
@@ -1,9 +1,17 @@
 package com.example.saladstar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class LogIn extends AppCompatActivity {
 
@@ -12,6 +20,32 @@ public class LogIn extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.log_in);
 
+        final TextView txtRegistrar = findViewById(R.id.txtRegistrar);
+        final EditText edtUsuario = findViewById(R.id.edtUsuario);
+        final EditText edtContrasenya = findViewById(R.id.edtContrasenya);
+        final Button btnInicioSesion = findViewById(R.id.btnInicioSesion);
 
+        txtRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LogIn.this, Register.class);
+                startActivity(intent);
+            }
+        });
+
+        btnInicioSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(edtUsuario.getText().toString().isEmpty()) {
+                    Toast.makeText(LogIn.this, "Introduce un Usuario", Toast.LENGTH_SHORT).show();
+                } else if(edtContrasenya.getText().toString().isEmpty()) {
+                    Toast.makeText(LogIn.this, "Introduce una Contraseña", Toast.LENGTH_SHORT).show();
+                } else {
+                    Intent intent = new Intent(LogIn.this, Home.class);
+                    intent.putExtra("usuario", edtUsuario.getText().toString());
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
