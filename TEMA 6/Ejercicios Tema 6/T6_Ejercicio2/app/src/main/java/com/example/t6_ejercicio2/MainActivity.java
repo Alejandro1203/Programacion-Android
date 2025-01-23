857fc8925d3c5f9596aab9336577b03599381752
Alejandro1203 <alefg2003@gmail.com>
2025-01-23 12:07:01 +0100

    Ejercicios Tema 6

---
+++ TEMA 6/Ejercicios Tema 6/T6_Ejercicio2/app/src/main/java/com/example/t6_ejercicio2/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.t6_ejercicio2;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager FM = getSupportFragmentManager();
+        FragmentTransaction FT = FM.beginTransaction();
+
+        Fragmento1 fragmento = new Fragmento1();
+        Fragmento2 fragmento2 = new Fragmento2();
+        FT.add(R.id.contenedor, fragmento);
+        FT.add(R.id.contenedor, fragmento);
+        FT.commit();
+    }
+}
