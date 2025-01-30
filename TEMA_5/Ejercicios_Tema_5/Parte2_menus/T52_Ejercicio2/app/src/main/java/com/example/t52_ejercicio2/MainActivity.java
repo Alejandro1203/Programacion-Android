c37ff1c02bdc58a073ab25e77fafb2652c62d4ce
Alejandro1203 <alefg2003@gmail.com>
2025-01-30 12:50:34 +0100

    Tema 5 Menus Ejercicio 2 Sin Terminar

---
+++ TEMA_5/Ejercicios_Tema_5/Parte2_menus/T52_Ejercicio2/app/src/main/java/com/example/t52_ejercicio2/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.t52_ejercicio2;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_encabezado, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int opcion =item.getItemId();
+    }
+}
