9d3e64016c705bd914b36105a80ef0b43948a026
Alejandro1203 <alefg2003@gmail.com>
2025-02-06 12:54:31 +0100

    Tema 7 Ejercicios 2 (Sin Terminar)

---
+++ TEMA_7/Ejercicios_Tema_7/T7_Ejercicio2/app/src/main/java/com/example/t7_ejercicio2/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.t7_ejercicio2;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final Button btn_definir = findViewById(R.id.btn_definir);
+        final Button btn_recuperar = findViewById(R.id.btn_recuperar);
+
+
+    }
+}
