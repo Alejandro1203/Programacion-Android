e25ec83588805f92af279d134f6f54368add1283
Alejandro1203 <alefg2003@gmail.com>
2025-02-06 10:48:53 +0100

    Tema 5 Menus Ejercicio 2

--- TEMA_5/Ejercicios_Tema_5/Parte2_menus/T52_Ejercicio2/app/src/main/java/com/example/t52_ejercicio2/MainActivity.java
+++ TEMA_5/Ejercicios_Tema_5/Parte2_menus/T52_Ejercicio2/app/src/main/java/com/example/t52_ejercicio2/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.t52_ejercicio2;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -28,6 +29,52 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        int opcion =item.getItemId();
+        int opcion = item.getItemId();
+
+        final TextView texto = findViewById(R.id.texto);
+
+        if(opcion == R.id.MnOp1_1) {
+            texto.setText("PULSADO LUNES");
+            return true;
+
+        } else if(opcion == R.id.MnOp1_2) {
+            texto.setText("PULSADO MARTES");
+            return true;
+
+        } else if(opcion == R.id.MnOp1_3) {
+            texto.setText("PULSADO MIÉRCOLES");
+            return true;
+
+        } else if(opcion == R.id.MnOp1_4) {
+            texto.setText("PULSADO JUEVES");
+            return true;
+
+        } else if(opcion == R.id.MnOp1_5) {
+            texto.setText("PULSADO VIERNES");
+            return true;
+
+        } else if(opcion == R.id.MnOp1_6) {
+            texto.setText("PULSADO SÁBADO");
+            return true;
+
+        } else if(opcion == R.id.MnOp1_7) {
+            texto.setText("PULSADO DOMINGO");
+            return true;
+
+        } else if(opcion == R.id.MnOp2_1) {
+            texto.setText("Pulsado el mes de enero");
+            return true;
+        } else if(opcion == R.id.MnOp2_2) {
+            texto.setText("Pulsado el mes de febrero");
+            return true;
+        } else if(opcion == R.id.MnOp2_3) {
+            texto.setText("Pulsado el mes de marzo");
+            return true;
+        } else if(opcion == R.id.MnOp2_4) {
+            texto.setText("Pulsado el mes de abril");
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
     }
 }
