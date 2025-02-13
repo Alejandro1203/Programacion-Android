cc9630a5481330d74e2451cd8d7fc9c99c607d47
Alejandro1203 <alefg2003@gmail.com>
2025-02-13 10:55:33 +0100

    T7_Ejercicio3

--- TEMA_7/Ejercicios_Tema_7/T7_Ejercicio3/app/src/main/java/com/example/t7_ejercicio3/MainActivity.java
+++ TEMA_7/Ejercicios_Tema_7/T7_Ejercicio3/app/src/main/java/com/example/t7_ejercicio3/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.t7_ejercicio3;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -14,5 +18,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final Button btn = findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, OpcionesActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
