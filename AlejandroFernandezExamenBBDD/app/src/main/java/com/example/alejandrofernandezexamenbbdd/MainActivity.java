abe5dd141626fd8abe11e9f10ceba891f82993d8
Alejandro1203 <alefg2003@gmail.com>
2025-02-17 12:22:51 +0100

    Examen (Punto b)

---
+++ AlejandroFernandezExamenBBDD/app/src/main/java/com/example/alejandrofernandezexamenbbdd/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.alejandrofernandezexamenbbdd;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId() == R.id.MnOp1) {
+            startActivity(new Intent(MainActivity.this, OpcionesActivity.class));
+
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+}
