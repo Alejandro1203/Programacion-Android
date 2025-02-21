fab6e1310d39b6b73e2f7e071df1db2eb983837c
Alejandro1203 <alefg2003@gmail.com>
2025-02-21 13:14:18 +0100

    El Huevazo

--- ElHuevazo/app/src/main/java/com/example/elhuevazo/MainActivity.java
+++ ElHuevazo/app/src/main/java/com/example/elhuevazo/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.elhuevazo;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,5 +18,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ImageView image = findViewById(R.id.huevo);
+        final EditText edit = findViewById(R.id.edt);
+        final Button btn = findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String tecto = edit.getText().toString();
+                String textoLimpio = tecto.replaceAll("\\s+", "").toLowerCase();
+
+                if(textoLimpio.contains("kinderbueno")) {
+                    image.setImageResource(R.drawable.bellako);
+                }
+            }
+        });
+
+
     }
 }
