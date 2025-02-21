b470ac607c8df3b50a5b65768e47cc54bec2d486
Alejandro1203 <alefg2003@gmail.com>
2025-02-21 14:07:06 +0100

    activity_main.xml

--- TEMA_7/PracticaEvaluacion_2Trimestre_Alejandro/app/src/main/java/com/example/practicaevaluacion_2trimestre_alejandro/ContactosProveedor.java
+++ TEMA_7/PracticaEvaluacion_2Trimestre_Alejandro/app/src/main/java/com/example/practicaevaluacion_2trimestre_alejandro/ContactosProveedor.java
@@ -7,35 +7,34 @@ import android.content.UriMatcher;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
-import android.os.CancellationSignal;
 import android.provider.BaseColumns;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-import com.example.practicaevaluacion_2trimestre_alejandro.BBDD.AvatarBBDD;
+import com.example.practicaevaluacion_2trimestre_alejandro.BBDD.ContactoBBDD;
 
-public class Proveedor extends ContentProvider {
+public class ContactosProveedor extends ContentProvider {
 
     // Definición de la URI
     private static final String AUTHORITY = "com.example.practicaevaluacion_2trimestre_alejandro";
-    private static final String URI = "content://" + AUTHORITY + "/avatares";
+    private static final String URI = "content://" + AUTHORITY + "/contactos";
     public static Uri CONTENT_URI = Uri.parse(URI);
 
     // Objeto UriMatcher
-    private static final int AVATAR = 1; // Acceso genérico a la tabla
-    private static final int AVATAR_ID = 2; // Acceso a una fila
+    private static final int CONTACTO = 1; // Acceso genérico a la tabla
+    private static final int CONTACTO_ID = 2; // Acceso a una fila
     private static final UriMatcher URI_MATCHER;
 
     static {
         URI_MATCHER = new UriMatcher(UriMatcher.NO_MATCH);
-        URI_MATCHER.addURI(AUTHORITY, "avatares", AVATAR);
-        URI_MATCHER.addURI(AUTHORITY, "avatares/#", AVATAR_ID);
+        URI_MATCHER.addURI(AUTHORITY, "contactos", CONTACTO);
+        URI_MATCHER.addURI(AUTHORITY, "contactos/#", CONTACTO_ID);
     }
 
-    public static final class Avatares implements BaseColumns {
+    public static final class Contactos implements BaseColumns {
 
-        private Avatares() {
+        private Contactos() {
 
         }
 
@@ -44,28 +43,28 @@ public class Proveedor extends ContentProvider {
         public static final String COL_AVATAR = "avatar";
     }
 
-    private AvatarBBDD avatarBBDD;
-    private static final String BD_NOMBRE = "DBAvatar";
+    private ContactoBBDD contactoBBDD;
+    private static final String BD_NOMBRE = "DBContacto";
     private static final int BD_VERSION = 1;
-    private static final String TABLA_AVATARES = "Avatares";
+    private static final String TABLA_CONTACTOS = "Contactos";
 
     @Override
     public boolean onCreate() {
-        avatarBBDD = new AvatarBBDD(getContext(), BD_NOMBRE, null, BD_VERSION);
+        contactoBBDD = new ContactoBBDD(getContext(), BD_NOMBRE, null, BD_VERSION);
 
         return true;
     }
 
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
-
-        // Si es una consulta a un ID concreto construimos el WHERE
         String where = selection;
-        if (URI_MATCHER.match(uri) == AVATAR_ID){
+
+        if (URI_MATCHER.match(uri) == CONTACTO_ID){
             where = "_id=" + uri.getLastPathSegment();
         }
-        SQLiteDatabase db = avatarBBDD.getWritableDatabase();
-        Cursor cursor = db.query(TABLA_AVATARES, projection, where, selectionArgs, null, null, sortOrder);
+
+        SQLiteDatabase db = contactoBBDD.getWritableDatabase();
+        Cursor cursor = db.query(TABLA_CONTACTOS, projection, where, selectionArgs, null, null, sortOrder);
 
         return cursor;
     }
@@ -76,10 +75,10 @@ public class Proveedor extends ContentProvider {
         int match = URI_MATCHER.match(uri);
 
         switch (match) {
-            case AVATAR:
-                return "vnd.android.cursor.dir/vnd.iesbelen.avatar";
-            case AVATAR_ID:
-                return "vnd.android.cursor.item/vnd.iesbelen.avatar";
+            case CONTACTO:
+                return "vnd.android.cursor.dir/vnd.iesbelen.contacto";
+            case CONTACTO_ID:
+                return "vnd.android.cursor.item/vnd.iesbelen.contacto";
             default:
                 return null;
         }
@@ -90,9 +89,9 @@ public class Proveedor extends ContentProvider {
     public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
         long regId = 1;
 
-        SQLiteDatabase db =avatarBBDD.getWritableDatabase();
+        SQLiteDatabase db = contactoBBDD.getWritableDatabase();
 
-        regId = db.insert(TABLA_AVATARES, null, values);
+        regId = db.insert(TABLA_CONTACTOS, null, values);
 
         Uri newUri = ContentUris.withAppendedId(CONTENT_URI, regId);
 
@@ -105,12 +104,12 @@ public class Proveedor extends ContentProvider {
 
         String where = selection;
 
-        if (URI_MATCHER.match(uri) == AVATAR_ID) {
+        if (URI_MATCHER.match(uri) == CONTACTO_ID) {
             where = "_id" + uri.getLastPathSegment();
         }
 
-        SQLiteDatabase db = avatarBBDD.getWritableDatabase();
-        cont = db.delete(TABLA_AVATARES, where,selectionArgs);
+        SQLiteDatabase db = contactoBBDD.getWritableDatabase();
+        cont = db.delete(TABLA_CONTACTOS, where,selectionArgs);
 
         return cont;
     }
@@ -121,12 +120,12 @@ public class Proveedor extends ContentProvider {
 
         String where = selection;
 
-        if (URI_MATCHER.match(uri) == AVATAR_ID){
+        if (URI_MATCHER.match(uri) == CONTACTO_ID){
             where = "_id=" + uri.getLastPathSegment();
         }
 
-        SQLiteDatabase db = avatarBBDD.getWritableDatabase();
-        cont = db.update(TABLA_AVATARES, values, where, selectionArgs);
+        SQLiteDatabase db = contactoBBDD.getWritableDatabase();
+        cont = db.update(TABLA_CONTACTOS, values, where, selectionArgs);
 
         return cont;
     }
--- TEMA_7/PracticaEvaluacion_2Trimestre_Alejandro/app/src/main/java/com/example/practicaevaluacion_2trimestre_alejandro/MainActivity.java
+++ TEMA_7/PracticaEvaluacion_2Trimestre_Alejandro/app/src/main/java/com/example/practicaevaluacion_2trimestre_alejandro/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -15,6 +16,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
     private void insertarDataBBDD(SQLiteDatabase db) {
