4ef1465c6b4dcee3074f393b9e1e0cee620c8492
Alejandro1203 <alefg2003@gmail.com>
2025-03-03 09:00:59 +0100

    Spinner

--- TEMA_7/PracticaEvaluacion_2Trimestre_Alejandro/app/src/main/java/com/example/practicaevaluacion_2trimestre_alejandro/MainActivity.java
+++ TEMA_7/PracticaEvaluacion_2Trimestre_Alejandro/app/src/main/java/com/example/practicaevaluacion_2trimestre_alejandro/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.practicaevaluacion_2trimestre_alejandro;
 
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +10,11 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.practicaevaluacion_2trimestre_alejandro.Spinner.AdaptadorSpinner;
+import com.example.practicaevaluacion_2trimestre_alejandro.Spinner.DatosSpinner;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +25,23 @@ public class MainActivity extends AppCompatActivity {
 
         final Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        final Spinner miSpinner = findViewById(R.id.spinner_avatar);
+
+        ArrayList<DatosSpinner> datosSpinner = new ArrayList<>();
+        datosSpinner.add(new DatosSpinner(R.drawable.batman));
+        datosSpinner.add(new DatosSpinner(R.drawable.capi));
+        datosSpinner.add(new DatosSpinner(R.drawable.deadpool));
+        datosSpinner.add(new DatosSpinner(R.drawable.hulk));
+        datosSpinner.add(new DatosSpinner(R.drawable.furia));
+        datosSpinner.add(new DatosSpinner(R.drawable.ironman));
+        datosSpinner.add(new DatosSpinner(R.drawable.lobezno));
+        datosSpinner.add(new DatosSpinner(R.drawable.spiderman));
+        datosSpinner.add(new DatosSpinner(R.drawable.thor));
+        datosSpinner.add(new DatosSpinner(R.drawable.wonderwoman));
+
+        AdaptadorSpinner adaptadorSpinner = new AdaptadorSpinner(this, datosSpinner);
+        miSpinner.setAdapter(adaptadorSpinner);
     }
 
     private void insertarDataBBDD(SQLiteDatabase db) {
--- TEMA_7/PracticaEvaluacion_2Trimestre_Alejandro/app/src/main/java/com/example/practicaevaluacion_2trimestre_alejandro/Spinner/AdaptadorSpinner.java
+++ TEMA_7/PracticaEvaluacion_2Trimestre_Alejandro/app/src/main/java/com/example/practicaevaluacion_2trimestre_alejandro/Spinner/AdaptadorSpinner.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -26,5 +27,28 @@ public class AdaptadorSpinner extends ArrayAdapter<DatosSpinner> {
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         LayoutInflater mostrado = LayoutInflater.from(getContext());
+        View elemento = mostrado.inflate(R.layout.elemento_spinner,parent,false);
+
+        ImageView avatar = elemento.findViewById(R.id.avatar);
+        avatar.setImageResource(datosSpinner.get(position).getAvatar());
+
+        return elemento;
+    }
+
+    @Override
+    public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        if(convertView==null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.elemento_spinner,parent,false);
+        }
+
+        ImageView avatar = convertView.findViewById(R.id.avatar);
+        DatosSpinner elemento = getItem(position);
+
+        if (elemento!=null){
+            avatar.setImageResource(elemento.getAvatar());
+        }
+
+        return convertView;
     }
 }
